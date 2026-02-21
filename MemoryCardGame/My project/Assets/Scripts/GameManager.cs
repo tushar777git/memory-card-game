c6b2339b74a2180e29a877503267a948d6bddb61
tusharRule14 <tushar.chaudhari@rule14.com>
2026-02-21 15:45:23 +0530

    memorycard

---
+++ MemoryCardGame/My project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,243 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    
+    public static int gameSize = 4;
+
+    
+    [SerializeField]
+    private GameObject prefab;
+
+    
+    [SerializeField]
+    private GameObject cardList;
+
+    
+    [SerializeField]
+    private Sprite cardBack;
+
+    
+    [SerializeField]
+    private Sprite[] sprites;
+
+   
+    private _Card[] cards;
+
+   
+    [SerializeField]
+    private GameObject panel;
+
+    
+
+    [SerializeField]
+    private GameObject info;
+
+   
+    [SerializeField]
+    private _Card spritePreload;
+
+   
+    [SerializeField]
+    private TMP_Text timeLabel;
+
+    private float time;
+
+    private int spriteSelected;
+    private int cardSelected;
+    private int cardLeft;
+    private bool gameStart;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        gameStart = false;
+        panel.SetActive(false);
+        
+    }
+
+    
+    public void StartCardGame()
+    {
+        if (gameStart) return;
+
+        gameStart = true;
+
+        panel.SetActive(true);
+        info.SetActive(false);
+
+        SetGamePanel();
+
+        cardSelected = -1;
+        spriteSelected = -1;
+        cardLeft = cards.Length;
+
+        SpriteCardAllocation();
+
+        StartCoroutine(HideFace());
+
+        time = 0;
+    }
+
+    
+
+    private void SetGamePanel()
+    {
+        int totalCards = gameSize * gameSize;
+        cards = new _Card[totalCards];
+
+        foreach (Transform child in cardList.transform)
+            Destroy(child.gameObject);
+
+        for (int i = 0; i < totalCards; i++)
+        {
+            GameObject c = Instantiate(prefab, cardList.transform);
+
+            cards[i] = c.GetComponent<_Card>();
+            cards[i].ID = i;
+        }
+    }
+
+
+    void ResetFace()
+    {
+        for (int i = 0; i < cards.Length; i++)
+            cards[i].ResetRotation();
+    }
+
+    
+    IEnumerator HideFace()
+    {
+        yield return new WaitForSeconds(0.3f);
+
+        for (int i = 0; i < cards.Length; i++)
+            cards[i].Flip();
+
+        yield return new WaitForSeconds(0.5f);
+    }
+
+   
+    private void SpriteCardAllocation()
+    {
+        int[] selectedID = new int[cards.Length / 2];
+
+        for (int i = 0; i < cards.Length / 2; i++)
+        {
+            int value = Random.Range(0, sprites.Length);
+
+            for (int j = i; j > 0; j--)
+            {
+                if (selectedID[j - 1] == value)
+                    value = (value + 1) % sprites.Length;
+            }
+
+            selectedID[i] = value;
+        }
+
+        for (int i = 0; i < cards.Length; i++)
+        {
+            cards[i].Active();
+            cards[i].SpriteID = -1;
+            cards[i].ResetRotation();
+        }
+
+        for (int i = 0; i < cards.Length / 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                int value = Random.Range(0, cards.Length);
+
+                while (cards[value].SpriteID != -1)
+                    value = (value + 1) % cards.Length;
+
+                cards[value].SpriteID = selectedID[i];
+            }
+        }
+    }
+
+    public Sprite GetSprite(int spriteId)
+    {
+        return sprites[spriteId];
+    }
+
+    public Sprite CardBack()
+    {
+        return cardBack;
+    }
+
+    public bool canClick()
+    {
+        return gameStart;
+    }
+
+    public void cardClicked(int spriteId, int cardId)
+    {
+        if (spriteSelected == -1)
+        {
+            spriteSelected = spriteId;
+            cardSelected = cardId;
+        }
+        else
+        {
+            if (spriteSelected == spriteId)
+            {
+                cards[cardSelected].Inactive();
+                cards[cardId].Inactive();
+                cardLeft -= 2;
+                CheckGameWin();
+            }
+            else
+            {
+                cards[cardSelected].Flip();
+                cards[cardId].Flip();
+            }
+
+            cardSelected = -1;
+            spriteSelected = -1;
+        }
+    }
+
+    private void CheckGameWin()
+    {
+        if (cardLeft == 0)
+        {
+            EndGame();
+            
+        }
+    }
+
+    private void EndGame()
+    {
+        gameStart = false;
+        panel.SetActive(false);
+    }
+
+    public void CloseGame()
+    {
+        EndGame();
+    }
+
+    public void DisplayInfo(bool i)
+    {
+        info.SetActive(i);
+    }
+
+    private void Update()
+    {
+        if (gameStart)
+        {
+            time += Time.deltaTime;
+            timeLabel.text = "Time: " + time.ToString("F1") + "s";
+        }
+    }
+}
---
+++ MemoryCardGame/My project/Assets/Scripts/_Card.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class _Card : MonoBehaviour
+{
+
+    private int spriteID;
+    private int id;
+    private bool flipped;
+    private bool turning;
+    [SerializeField]
+    private Image img;
+
+    
+    private IEnumerator Flip90(Transform thisTransform, float time, bool changeSprite)
+    {
+        Quaternion startRotation = thisTransform.rotation;
+        Quaternion endRotation = thisTransform.rotation * Quaternion.Euler(new Vector3(0, 90, 0));
+        float rate = 1.0f / time;
+        float t = 0.0f;
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime * rate;
+            thisTransform.rotation = Quaternion.Slerp(startRotation, endRotation, t);
+
+            yield return null;
+        }
+       
+        if (changeSprite)
+        {
+            flipped = !flipped;
+            ChangeSprite();
+            StartCoroutine(Flip90(transform, time, false));
+        }
+        else
+            turning = false;
+    }
+   
+    public void Flip()
+    {
+        turning = true;
+      
+        StartCoroutine(Flip90(transform, 0.25f, true));
+    }
+   
+    private void ChangeSprite()
+    {
+        if (spriteID == -1 || img == null) return;
+        if (flipped)
+            img.sprite = GameManager.Instance.GetSprite(spriteID);
+        else
+            img.sprite = GameManager.Instance.CardBack();
+    }
+    
+    public void Inactive()
+    {
+        StartCoroutine(Fade());
+    }
+    
+    private IEnumerator Fade()
+    {
+        float rate = 1.0f / 2.5f;
+        float t = 0.0f;
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime * rate;
+            img.color = Color.Lerp(img.color, Color.clear, t);
+
+            yield return null;
+        }
+    }
+   
+    public void Active()
+    {
+        if (img)
+            img.color = Color.white;
+    }
+    
+    public int SpriteID
+    {
+        set
+        {
+            spriteID = value;
+            flipped = true;
+            ChangeSprite();
+        }
+        get { return spriteID; }
+    }
+    
+    public int ID
+    {
+        set { id = value; }
+        get { return id; }
+    }
+    
+    public void ResetRotation()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+        flipped = true;
+    }
+   
+    public void CardBtn()
+    {
+        if (flipped || turning) return;
+        if (!GameManager.Instance.canClick()) return;
+        Flip();
+        StartCoroutine(SelectionEvent());
+    }
+    
+    private IEnumerator SelectionEvent()
+    {
+        yield return new WaitForSeconds(0.5f);
+        GameManager.Instance.cardClicked(spriteID, id);
+    }
+}
